84c8f51bee06baf8a5d9abbf2558296c37045d2d
ShogunSkinoski <whitebirdsdk@gmail.com>
2024-01-26 17:53:03 +0300

    Delete unused code related to fly detection

--- modules/flies/app/routes/flies.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Depends, HTTPException, File, UploadFile
-from flies.domain.flies import Flies
-from flies.infra.ai.detection import FlyDetectionService
-fly = APIRouter()
-
-@fly.post("")
-async def detect_fly(image: UploadFile = File(...), fly_detection_service: FlyDetectionService = Depends()):
-    return fly_detection_service.detect(image)
