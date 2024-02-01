b6663254902c52660cc37c8b3b1a29af3bfec9b5
ShogunSkinoski <whitebirdsdk@gmail.com>
2024-02-01 22:21:11 +0300

    Refactor fly detection API endpoint

--- src/modules/detection/app/flies.py
+++ src/modules/detection/app/flies.py
@@ -1,19 +1,48 @@
-from fastapi import APIRouter, Depends, HTTPException, File, UploadFile
+from fastapi import APIRouter, Body, Depends, File, UploadFile
+from fastapi.responses import JSONResponse
 from ultralytics import YOLO
+
 from modules.detection.app.services.fly_service import  AsyncFlyDetectionService
 from modules.detection.app.services.fly_service import  AsyncFlyDetectionAdapter
 from modules.detection.app.response import TotalFlyResponseMobile
+from modules.detection.infra.db.adapter.uow import SQLAlchemyUnitOfWorkManager
+from modules.detection.infra.db.model.flies_model import FliesModel
+from modules.detection.infra.db.adapter.repository import FliesRepositoryAdapter
+from sqlalchemy import create_engine
+
 import numpy as np
 from PIL import Image
-model = YOLO(r'src\utils\fly\model\runs\detect\train\weights\best.pt', verbose=False)
+
+model = YOLO(r'src\modules\detection\ai_models\best.pt', verbose=False)
 
 fly = APIRouter()
+
+def get_db():
+    return create_engine("mysql+mysqlconnector://root:perhap5gell@localhost:3306/ai4agriculture")
 
 def get_fly_detection_service():
     return AsyncFlyDetectionService(AsyncFlyDetectionAdapter(model))
 
+def get_fly_repository():
+    return FliesRepositoryAdapter()
+def get_uow():
+    return SQLAlchemyUnitOfWorkManager(get_db())
+
 @fly.post("")
-async def detect_fly(image: UploadFile = File(...), service : AsyncFlyDetectionService = Depends(get_fly_detection_service)):
+async def detect_fly(image: UploadFile = File(...),
+                    sector_id: int = Body(...),
+                    greenhouse_id: int = Body(...),
+                    service : AsyncFlyDetectionService = Depends(get_fly_detection_service),
+                    repository : FliesRepositoryAdapter = Depends(get_fly_repository),
+                    uow : SQLAlchemyUnitOfWorkManager = Depends(get_uow)):
     image = Image.open(image.file)
     total_flies_response = await service.execute(np.array(image), TotalFlyResponseMobile)
-    return total_flies_response.serialize()
+
+    flies_model = FliesModel(
+                            flies_count=total_flies_response.total_fly,
+                            created_at= total_flies_response.date,
+                            greenhouse_id=greenhouse_id,
+                            sector_id=sector_id
+                            )
+    repository.add(flies_model, uow)
+    return JSONResponse(total_flies_response.serialize(), status_code=200, media_type="application/json")
