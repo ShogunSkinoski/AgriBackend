e95052fb9a53c48337002ea248146285be6f0332
ShogunSkinoski <whitebirdsdk@gmail.com>
2024-02-01 15:30:33 +0300

    Remove unused FliesAI class

--- src/modules/detection/infra/ai/detection.py
+++
@@ -1,6 +0,0 @@
-from modules.detection.domain.flies import Flies
-
-class FliesAI:
-    
-    def detect(self, image):
-        return Flies.from_dict({"greenhouse_id": 1, "sector_id": 1, "total_flies": 1, "created_at": "2021-01-01"})
